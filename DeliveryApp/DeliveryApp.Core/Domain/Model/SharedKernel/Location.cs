d3188483ff25e5750d08ecd70ee947438578fccb
Denis Ponomarev <denis.ponomarev.1989@yandex.ru>
2025-04-22 16:06:34 +0300

    Unit-тесты для Transport.

--- DeliveryApp/DeliveryApp.Core/Domain/Model/SharedKernel/Location.cs
+++ DeliveryApp/DeliveryApp.Core/Domain/Model/SharedKernel/Location.cs
@@ -5,37 +5,35 @@ namespace DeliveryApp.Core.Domain.Model.SharedKernel;
 
 public class Location : ValueObject
 {
-    private const byte MIN_X = 1;
-    private const byte MAX_X = 10;
-    private const byte MIN_Y = 1;
-    private const byte MAX_Y = 10;
+    private const int MIN_X = 1;
+    private const int MAX_X = 10;
+    private const int MIN_Y = 1;
+    private const int MAX_Y = 10;
 
     [ExcludeFromCodeCoverage]
     private Location()
     {
     }
 
-    private Location(byte x, byte y)
+    private Location(int x, int y)
     {
         X = x;
         Y = y;
     }
 
-    public byte X { get; }
-    public byte Y { get; }
+    public int X { get; }
+    public int Y { get; }
 
     public int DistanceTo(Location location)
     {
+        ArgumentNullException.ThrowIfNull(location);
         return Math.Abs(X - location.X) + Math.Abs(Y - location.Y);
     }
 
-    public static Location Create(byte x, byte y)
+    public static Location Create(int x, int y)
     {
-        if (x is < MIN_X or > MAX_X)
-            throw new ArgumentOutOfRangeException(nameof(x));
-
-        if (y is < MIN_Y or > MAX_Y)
-            throw new ArgumentOutOfRangeException(nameof(y));
+        if (x is < MIN_X or > MAX_X) throw new ArgumentOutOfRangeException(nameof(x));
+        if (y is < MIN_Y or > MAX_Y) throw new ArgumentOutOfRangeException(nameof(y));
 
         return new(x, y);
     }
@@ -43,8 +41,8 @@ public class Location : ValueObject
     public static Location CreateRandom()
     {
         var random = new Random();
-        var x = (byte)random.Next(MIN_X, MAX_X + 1);
-        var y = (byte)random.Next(MIN_Y, MAX_Y + 1);
+        var x = random.Next(MIN_X, MAX_X + 1);
+        var y = random.Next(MIN_Y, MAX_Y + 1);
         return new(x, y);
     }
 
