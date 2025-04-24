0b3e81737c8f9afb8ecbefe52e872dd79452b655
Denis Ponomarev <denis.ponomarev.1989@yandex.ru>
2025-04-24 12:29:03 +0300

    Order and unit-tests.

--- DeliveryApp/DeliveryApp.Core/Domain/Model/OrderAggregate/Order.cs
+++ DeliveryApp/DeliveryApp.Core/Domain/Model/OrderAggregate/Order.cs
@@ -1,4 +1,5 @@
 using System.Diagnostics.CodeAnalysis;
+using DeliveryApp.Core.Domain.Model.CourierAggregate;
 using DeliveryApp.Core.Domain.Model.SharedKernel;
 using Primitives;
 
@@ -9,13 +10,42 @@ public class Order : Aggregate<Guid>
     [ExcludeFromCodeCoverage]
     private Order() { }
 
-    private Order(Location location)
+    public Order(Guid basketId, Location location)
     {
-        Id = Guid.NewGuid();
+        if (basketId == Guid.Empty)
+            throw new ArgumentException(null, nameof(basketId));
+
+        ArgumentNullException.ThrowIfNull(location);
+
+        Id = basketId;
         Location = location;
+        Status = OrderStatus.Created;
     }
 
-    public Guid CourierId { get; private set; }
+    public Guid? CourierId { get; private set; }
     public OrderStatus Status { get; private set; }
     public Location Location { get; private set; }
+
+    public void Assigne(Courier courier)
+    {
+        ArgumentNullException.ThrowIfNull(courier);
+
+        if (CourierId.HasValue && CourierId != Guid.Empty)
+            throw new Exception("Courier already assigned");
+
+        if (Status != OrderStatus.Created)
+            throw new Exception($"The order must have the status '{OrderStatus.Created.Name}'");
+        
+        CourierId = courier.Id;
+        Status = OrderStatus.Assigned;
+    }
+
+    public void Complete()
+    {
+        if (Status != OrderStatus.Assigned)
+            throw new Exception($"The order must have the status '{OrderStatus.Assigned.Name}'");
+        
+        Status = OrderStatus.Completed;
+        CourierId = null;
+    }
 }
