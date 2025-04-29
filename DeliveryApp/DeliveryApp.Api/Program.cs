ced828b903ba7bc846f900d516326e782ecb060c
Denis Ponomarev <denis.ponomarev.1989@yandex.ru>
2025-04-29 18:01:16 +0300

    Сервис DispatchService зарегистрировал как Singleton.

--- DeliveryApp/DeliveryApp.Api/Program.cs
+++ DeliveryApp/DeliveryApp.Api/Program.cs
@@ -6,7 +6,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
-builder.Services.AddTransient<IDispatchService, DispatchService>();
+builder.Services.AddSingleton<IDispatchService, DispatchService>();
 
 var app = builder.Build();
 
